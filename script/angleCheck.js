6939b537ebc242a533743f8767de9682446f7a83
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2020-12-29 14:38:50 +0530

    feat: code update

--- script/angleCheck.js
+++ script/angleCheck.js
@@ -1,20 +1,23 @@
-var angleOne = parseInt(document.querySelector("#number1").value);
-var angleTwo = parseInt(document.querySelector("#number2").value);
-var angleThree = parseInt(document.querySelector("#number3").value);
+
 var submitButton = document.querySelector("#angleCheck");
-var resultbox=document.querySelector("#result");
+var resultbox = document.querySelector("#result");
 
-var total = angleOne+angleTwo+angleThree;
-console.log(total);
 
-const angleChecker=()=>{
-   
-    if(total === 180){
+
+
+const angleChecker = () => {
+
+    var angleOne = document.querySelector("#number1");
+    var angleTwo = document.querySelector("#number2");
+    var angleThree = document.querySelector("#number3");
+    var total = parseInt(angleOne.value) + parseInt(angleTwo.value) + parseInt(angleThree.value);
+
+    if (total === 180) {
 
         resultbox.innerHTML = "yes !!! It is a triangle "
 
-    }else {
-         
+    } else {
+
         resultbox.innerHTML = "Nope !!! It is not a triangle "
 
     }
@@ -25,4 +28,4 @@ const angleChecker=()=>{
 
 
 
-submitButton.addEventListener('click',angleChecker)
+// submitButton.addEventListener('click',angleChecker)
