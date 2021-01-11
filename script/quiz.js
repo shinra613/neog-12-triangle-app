ab7510a12e13c519543df55a04c764a631273aca
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2021-01-11 12:16:12 +0530

    refactor

--- script/quiz.js
+++ script/quiz.js
@@ -1,47 +1,73 @@
-var question = document.querySelector("#question")
-
-var questionBank = [
-    {
-        sn:"1",
-        que:"who the legend",
-        opt:["bill","kill"],
-        ans: "billa"
-    },
-    {   
-        sn:"2",
-        que:"who the bajend",
-        opt:["bill","kill"],
-        ans: "killa"
-    },
-]
+var question = document.querySelector("#question");
+let form = document.querySelector('#qform');
 
 
 
-window.onload = function() {
-    
-    
+
+
+
+window.onload = function () {
+
+    var questionBank = [
+        {
+            sn: "1",
+            que: "test question loren ipsum yo",
+            opt: ["bill", "kill", "bilf", "kilf"],
+            ans: "bill"
+        },
+        {
+            sn: "2",
+            que: "test question loren ipsum yo",
+            opt: ["bill", "kill", "bil", "kil"],
+            ans: "kill"
+        },
+       
+    ]
+
+
     console.log(questionBank[0].que);
 
-    for(var i =0;i<=questionBank.length;i++){
-        
-        var qt = document.createElement("div"); 
-       
-        
-        
+    for (var i = 0; i <= questionBank.length; i++) {
+
+        var qt = document.createElement("div");
+
+
+
 
         qt.classList.add("qstyle");
-        qt.innerText = questionBank[i].sn+ ". " + questionBank[i].que;
+        qt.innerText = questionBank[i].sn + ". " + questionBank[i].que;
         question.appendChild(qt);
 
-        for(var j=0;j<questionBank[i].opt.length;j++){
+        var optionContainer = document.createElement("div");
 
-               var od = document.createElement("div"); 
-               od.innerHTML= "<input type=radio value = "+ questionBank[i].opt[j] +"  >"+ "<label>"+ questionBank[i].opt[j]+"</label>"; 
-               
-               question.appendChild(od)
+        for (var j = 0; j < questionBank[i].opt.length; j++) {
+
+            var od = document.createElement("span");
+            od.innerHTML = "<input type=radio value = " + questionBank[i].opt[j] + "  >" +  questionBank[i].opt[j];
+
+            optionContainer.appendChild(od);
 
         }
+
+        question.appendChild(optionContainer);
+ 
+
+       
+
     }
+
+   
+   
+}
+
+
+console.log(form.elements);
+
+
+const answerCheck=()=>{
+
+   
+
 }
 
 
